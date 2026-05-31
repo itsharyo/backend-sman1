780bb257c002791117cc05d0a4313dffcde87102
Radityo Haryo <itsharyo@gmail.com>
2026-06-01 02:24:05 +0700

    Fix import case for User model in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User'); // Memanggil kerangka User yang barusan kamu buat
+const User = require('../models/user'); // Memanggil kerangka User yang barusan kamu buat
 
 // Jalur untuk Registrasi (Daftar Akun Baru)
 router.post('/register', async (req, res) => {
