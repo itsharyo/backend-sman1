3718d2ae98383e4538973b7e91359c7ec15f6655
itsharyo <itsharyo@gmail.com>
2026-06-01 13:57:00 +0700

    Menambahkan pesan selamat datang di jalur utama

--- server.js
+++ server.js
@@ -14,10 +14,16 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Terhubung"))
   .catch(err => console.log("Gagal koneksi:", err.message));
 
+// --- JALUR UTAMA (ROOT) ---
+app.get('/', (req, res) => {
+  res.send('Selamat datang di API Backend Sistem Informasi Akademik SMAN 1. Server Aktif! 🚀');
+});
+
 // --- MENGHUBUNGKAN ROUTES (JALUR API) ---
 // Jalur ini akan mengarah ke file auth.js di dalam folder routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/siswa', require('./routes/siswa'));
+
 // --- JALANKAN SERVER ---
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
